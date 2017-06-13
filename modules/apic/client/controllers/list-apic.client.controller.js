2727742b18e21ba6d5e7ce53f4fb3c5b4dfd5ac0
rohscx <R_E__g_@Macintosh.local>
2017-06-13 18:33:05 -0400

    test

--- modules/apic/client/controllers/list-apic.client.controller.js
+++ modules/apic/client/controllers/list-apic.client.controller.js
@@ -10,6 +10,9 @@
   function ApicListController(ApicService) {
     var vm = this;
     console.log(this);
+    str = JSON.stringify(this);
+str = JSON.stringify(this, null, 4); // (Optional) beautiful indented output.
+console.log(str); // Logs output to dev tools console.
 
     vm.apic = ApicService.query();
   }
