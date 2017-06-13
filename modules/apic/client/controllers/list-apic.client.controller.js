ff9d17f5bb4467270314b0935efd0dfbfb5a53b3
rohscx <R_E__g_@Macintosh.local>
2017-06-13 18:35:43 -0400

    this

--- modules/apic/client/controllers/list-apic.client.controller.js
+++ modules/apic/client/controllers/list-apic.client.controller.js
@@ -9,7 +9,7 @@
 
   function ApicListController(ApicService) {
     var vm = this;
-    console.log(this);
+    console.log(ApicService.query());
     var str = JSON.stringify(this);
 str = JSON.stringify(this, null, 4); // (Optional) beautiful indented output.
 console.log(str); // Logs output to dev tools console.
